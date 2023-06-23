13d3ebc9b666691794fed9c634aa07ec1e0fdd2e
BIT <BIT@DESKTOP-EJDVL85>
2023-06-23 17:33:10 +0900

    반복문 final

---
+++ chapter01/src/main/java/chapter01/Ex28Gugudan2.java
@@ -0,0 +1,25 @@
+package chapter01;
+
+public class Ex28Gugudan2 {
+
+	public static void main(String[] args) {
+		// System.out.println("hello world");
+		System.out.print("hello world" + "\n");
+		
+		System.out.println("======");
+		
+		System.out.print("1 x 1 = 1");
+		System.out.print("\t");
+		System.out.print("2 x 1 = 2");
+		
+		System.out.print("\n");
+		
+		System.out.print("1 x 2 = 2");
+		System.out.print("\t");
+		System.out.print("2 x 2 = 4");
+		
+		
+		
+	}
+
+}
