0acb962647bc0349d3afb10f7cdd6b241114717c
BIT <BIT@DESKTOP-EJDVL85>
2023-06-28 15:59:34 +0900

    practice03~practice05

---
+++ practice03/src/main/java/prob03/CurrencyConverter.java
@@ -0,0 +1,17 @@
+package prob03;
+
+public class CurrencyConverter {
+
+	public static void setRate(double rate) {
+		
+	}
+
+	public static double toDollar(double won) {
+		return 0;
+	}
+
+	public static double toKRW(double dollar) {
+		return 0;
+	}
+
+}
