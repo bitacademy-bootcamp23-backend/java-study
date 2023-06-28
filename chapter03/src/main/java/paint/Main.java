6416bb68ffdd1f51181a241fd5a554c2afb0fe18
BIT <BIT@DESKTOP-EJDVL85>
2023-06-28 14:12:39 +0900

    인터페이스

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -37,6 +37,9 @@ public class Main {
 		Circle circle = new Circle();
 		// drawShape(circle);
 		draw(circle);
+		
+		GraphicText gt = new GraphicText("Hello World");
+		draw(gt);
 	}
 	
 	public static void draw(Drawable drawable) {
