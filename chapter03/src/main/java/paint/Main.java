c37a2ce6c79f1148fc1e5d8f7f106c464c9b37ea
BIT <BIT@DESKTOP-EJDVL85>
2023-06-28 13:54:11 +0900

    추상클래스&인터페이스

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -5,11 +5,13 @@ public class Main {
 		Point point1 = new Point();
 		point1.setX(100);
 		point1.setY(50);
-		drawPoint(point1);
+		// drawPoint(point1);
+		draw(point1);
 		point1.show(false);
 		
 		Point point2 = new Point(200, 150);
-		drawPoint(point2);
+		// drawPoint(point2);
+		draw(point2);
 		point2.show(false);
 		
 //		Point point3 = new ColorPoint();
@@ -19,15 +21,45 @@ public class Main {
 		point3.setX(200);
 		point3.setY(100);
 		((ColorPoint)point3).setColor("red");
-		drawPoint(point3);
+		// drawPoint(point3);
+		draw(point3);
+		
+		Rectangle rectangle = new Rectangle();
+		// drawRectangle(rectangle);
+		// drawShape(rectangle);
+		draw(rectangle);
+		
+		Triangle triangle = new Triangle();
+		// drawTriangle(triangle);
+		// drawShape(triangle);
+		draw(triangle);
+		
+		Circle circle = new Circle();
+		// drawShape(circle);
+		draw(circle);
 	}
 	
-	public static void drawPoint(Point point) {
-		point.show(true);
+	public static void draw(Drawable drawable) {
+		drawable.draw();
 	}
+	
+//	public static void drawPoint(Point point) {
+//		point.show(true);
+//	}
+
+//	public static void drawShape(Shape shape) {
+//		shape.draw();
+//	}
+	
+//	public static void drawRectangle(Rectangle rectangle) {
+//		rectangle.draw();
+//	}
+//
+//	public static void drawTriangle(Triangle triangle) {
+//		triangle.draw();
+//	}
 	
 //	public static void drawColorPoint(ColoPoint colorPoint) {
 //		colorPoint.show();
-//	}
-	
+//	}	
 }
