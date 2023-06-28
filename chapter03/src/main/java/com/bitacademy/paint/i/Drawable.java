880e4d2310d6741e619227df3f918347456521ae
BIT <BIT@DESKTOP-EJDVL85>
2023-06-28 15:18:21 +0900

    package

--- chapter03/src/main/java/com/bitacademy/paint/i/Drawable.java
+++ chapter03/src/main/java/com/bitacademy/paint/i/Drawable.java
@@ -1,4 +1,4 @@
-package paint;
+package com.bitacademy.paint.i;
 
 public interface Drawable {
 	void draw();
--- chapter03/src/main/java/com/bitacademy/paint/shape/Shape.java
+++ chapter03/src/main/java/com/bitacademy/paint/shape/Shape.java
@@ -1,4 +1,6 @@
-package paint;
+package com.bitacademy.paint.shape;
+
+import com.bitacademy.paint.i.Drawable;
 
 public abstract class Shape implements Drawable {
 	private String lineColor;
