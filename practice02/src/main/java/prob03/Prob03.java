21bb149c8e43a0fd14c0542f7f2c0ed475fa0895
비트부트캠프 <137239441+bitacademy-bootcamp23-backend@users.noreply.github.com>
2023-06-28 16:21:28 +0900

    Update Prob03.java

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -14,12 +14,15 @@ public class Prob03 {
 		printCharArray(c);
 	}
 
-	private static void replaceSpace(char[] c) {
+	public static void replaceSpace(char[] c) {
+		for(int i = 0; i < c.length; i++) {
+			if(c[i] == ' ') {
+				c[i] = ',';
+			}
+		}
 	}
 
-	private static void printCharArray(char[] c) {
+	public static void printCharArray(char[] c) {
 		System.out.println(c);
 	}
-	
-	
 }
