22c561cda066ad26b6565734bed73b06488fcb71
BIT <BIT@DESKTOP-EJDVL85>
2023-06-29 13:55:38 +0900

    String Class I

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,20 @@
+package chapter04;
+
+public class StringTest01 {
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\\temp");
+		
+		// "hello"
+		System.out.println("\"hello\"");
+		
+		// \t: tab
+		// \r: carrige return
+		// \n: newline
+		// \b: beep
+		
+		// '
+		char c ='\'';
+		System.out.println(c);		
+	}
+}
