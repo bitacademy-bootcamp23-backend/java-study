1a1b69a4d968e7856fd51703286fe1bfbcdbafa0
BIT <BIT@DESKTOP-EJDVL85>
2023-06-29 11:50:51 +0900

    Object II

--- chapter04/src/main/java/chapter04/Point.java
+++ chapter04/src/main/java/chapter04/Point.java
@@ -1,5 +1,7 @@
 package chapter04;
 
+import java.util.Objects;
+
 public class Point {
 	private int x;
 	private int y;
@@ -9,8 +11,28 @@ public class Point {
 		this.y = y;
 	}
 
+	
 	@Override
 	public String toString() {
 		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		return x == other.x && y == other.y;
 	}
 }
